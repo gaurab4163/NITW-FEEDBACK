6bf307a2e358e6ae4ed8abd45e5bf92202e5f872
Gaurab Manandhar <gaurab4163@gmail.com>
2018-11-21 21:25:12 +0530

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,33 @@
+var minRating=2; 	//min can be 1
+var maxRating=5; 	//max can be 5
+
+var courseIndex=0;
+
+var scope = angular.element('[ng-controller=feedbackCourses]').scope();
+
+var currentCourse=scope.courseName;
+
+for(var i=0;i<scope.course.length;i++){
+	if(scope.course[i].courseName==currentCourse)
+		courseIndex=i;
+}
+
+function randomizeRating(){
+	return Math.floor(Math.random()*(maxRating-minRating+1))+minRating;
+
+}
+
+for(var i = 0; i< scope.course[courseIndex].question.course.length; i++){
+	scope.course[courseIndex].question.course[i].display = true;
+	scope.course[courseIndex].question.course[i].value = randomizeRating();
+}
+
+for(var i = 0; i< scope.course[courseIndex].question.faculty.length; i++){
+	scope.course[courseIndex].question.faculty[i].display = true;
+	scope.course[courseIndex].question.faculty[i].value = randomizeRating();
+}
+
+scope.$apply();
+
+
+// $scope.submitFeedback();
