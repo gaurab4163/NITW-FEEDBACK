0c98ff460505b9ee39aae3875af3784f5acf205b
Gaurab Manandhar <gaurab4163@gmail.com>
2019-04-30 21:06:44 +0530

    updated for new django based student portal

--- feedback.js
+++ feedback.js
@@ -1,3 +1,33 @@
+/*code for new django based student portal*/
+
+var minRating=4; 	//min can be 1
+var maxRating=5; 	//max can be 5
+function randomizeRating(){
+	return Math.floor(Math.random()*(maxRating-minRating+1))+minRating;
+}
+
+for(var i = 1; i< 16; i++){
+	var questionNumber=i;
+	var feedback = document.getElementsByName("courseFeedback" + questionNumber);
+	feedback[0].value=randomizeRating();
+	if(i!=15)
+	courseFeedbackChanged(i);
+}
+
+
+for(var i = 1; i< 21; i++){
+	var questionNumber=i;
+	var feedback = document.getElementsByName("facultyFeedback" + questionNumber);
+	feedback[0].value=randomizeRating();
+
+	facultyFeedbackChanged(i);
+}
+
+
+
+/*
+//Feedback code for old PHP based student portal
+
 var minRating=5; 	//min can be 1
 var maxRating=5; 	//max can be 5
 
@@ -31,3 +61,5 @@ scope.$apply();
 
 
 // scope.submitFeedback();
+
+*/
